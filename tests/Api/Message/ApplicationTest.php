f3952120932dc2059e74952cd54574231e916ec7
Serhiy Lunak <serhiy.lunak@gmail.com>
2020-06-11 08:51:45 +0100

    Fix copyright header

--- tests/Api/Message/ApplicationTest.php
+++ tests/Api/Message/ApplicationTest.php
@@ -3,10 +3,10 @@
 /*
  * This file is part of the Pushover package.
  *
- *  (c) Serhiy Lunak <https://github.com/slunak>
+ * (c) Serhiy Lunak <https://github.com/slunak>
  *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Api\Message;
