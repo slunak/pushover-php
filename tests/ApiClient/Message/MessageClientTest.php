c6a2886b0642d99c1ea100620f775b4ce188fad0
Serhiy Lunak <serhiy.lunak@gmail.com>
2020-07-02 16:07:44 +0100

    Rewrite MessageClient tests

--- tests/ApiClient/Message/MessageClientTest.php
+++ tests/ApiClient/Message/MessageClientTest.php
@@ -12,12 +12,16 @@
 namespace ApiClient\Message;
 
 use PHPUnit\Framework\TestCase;
+use Serhiy\Pushover\Api\Message\Attachment;
 use Serhiy\Pushover\Api\Message\Message;
 use Serhiy\Pushover\Api\Message\Notification;
+use Serhiy\Pushover\Api\Message\Priority;
+use Serhiy\Pushover\Api\Message\Sound;
 use Serhiy\Pushover\ApiClient\Message\MessageClient;
 use Serhiy\Pushover\ApiClient\Message\MessageResponse;
 use Serhiy\Pushover\ApiClient\Request;
 use Serhiy\Pushover\Application;
+use Serhiy\Pushover\Exception\LogicException;
 use Serhiy\Pushover\Recipient;
 
 /**
@@ -51,17 +55,39 @@ class MessageClientTest extends TestCase
         return $notification;
     }
 
-    /**
-     * @depends testSend
-     * @param Notification $notification
-     */
-    public function testBuildCurlPostFields(Notification $notification)
+    public function testBuildCurlPostFields()
     {
+        $application = new Application("azGDORePK8gMaC0QOYAMyEEuzJnyUi"); // using dummy token
+        $recipient = new Recipient("uQiRzpo4DXghDmr9QzzfQu27cmVRsG"); // using dummy user key
+        $message = new Message("This is a test message", "This is a title of the message");
+        $message->setUrl("https://www.example.com");
+        $message->setUrlTitle("Example.com");
+        $message->setPriority(new Priority(Priority::EMERGENCY, 30, 300));
+        $message->getPriority()->setCallback("https://callback.example.com");
+        $message->setIsHtml(true);
+
+        $recipient->addDevice("ios");
+        $recipient->addDevice("android");
+
+        $notification = new Notification($application, $recipient, $message);
+        $notification->setSound(new Sound(Sound::PUSHOVER));
+
         $client = new MessageClient();
         $curlPostFields = $client->buildCurlPostFields($notification);
 
         $this->assertIsArray($curlPostFields);
-        $this->assertArrayHasKey("message", $curlPostFields);
-        $this->assertEquals("This is a test message", $curlPostFields["message"]);
+        $this->assertEquals("azGDORePK8gMaC0QOYAMyEEuzJnyUi", $curlPostFields['token']);
+        $this->assertEquals("uQiRzpo4DXghDmr9QzzfQu27cmVRsG", $curlPostFields['user']);
+        $this->assertEquals("This is a test message", $curlPostFields['message']);
+        $this->assertEquals("ios,android", $curlPostFields['device']);
+        $this->assertEquals("This is a title of the message", $curlPostFields['title']);
+        $this->assertEquals("https://www.example.com", $curlPostFields['url']);
+        $this->assertEquals("Example.com", $curlPostFields['url_title']);
+        $this->assertEquals("2", $curlPostFields['priority']);
+        $this->assertEquals("30", $curlPostFields['retry']);
+        $this->assertEquals("300", $curlPostFields['expire']);
+        $this->assertEquals("https://callback.example.com", $curlPostFields['callback']);
+        $this->assertEquals("1", $curlPostFields['html']);
+        $this->assertEquals("pushover", $curlPostFields['sound']);
     }
 }
