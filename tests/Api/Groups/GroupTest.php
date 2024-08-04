65086573051e717b3c1ee74de7693f39b2135517
Serhiy Lunak <8351121+slunak@users.noreply.github.com>
2024-08-04 09:17:14 +0100

    Update tests/Api/Groups/GroupTest.php
    Co-authored-by: Oskar Stark <oskarstark@googlemail.com>

--- tests/Api/Groups/GroupTest.php
+++ tests/Api/Groups/GroupTest.php
@@ -70,7 +70,8 @@ class GroupTest extends TestCase
     /**
      * @group Integration
      */
-    public function testCreate() {
+    public function testCreate()
+    {
         $application = new Application("cccc3333CCCC3333dddd4444DDDD44"); // using dummy token
         $group = new Group("eeee5555EEEE5555ffff6666FFFF66", $application);
 
