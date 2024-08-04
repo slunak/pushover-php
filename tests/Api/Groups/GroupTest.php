7e41f361b0d6d8783c1310d34108b79e2feb5431
Cybervitexus <info@vitexsoftware.cz>
2024-08-04 08:54:03 +0200

    #26 - Create Group Added into Integration test
    The Group Name now contains date & time to avoid conflict with already existing group.

--- tests/Api/Groups/GroupTest.php
+++ tests/Api/Groups/GroupTest.php
@@ -67,14 +67,15 @@ class GroupTest extends TestCase
         $this->assertInstanceOf(RetrieveGroupResponse::class, $response);
     }
     
-    public function testCreate()
-    {
+    /**
+     * @group Integration
+     */
+    public function testCreate() {
         $application = new Application("cccc3333CCCC3333dddd4444DDDD44"); // using dummy token
         $group = new Group("eeee5555EEEE5555ffff6666FFFF66", $application);
 
-        $response = $group->create('test');
+        $response = $group->create('unit test '. date('Y-m-d H:i:s'));
 
         $this->assertInstanceOf(CreateGroupResponse::class, $response);
-    }
-    
+    }    
 }
