ab1f723aca7694a4bb017f9e66d4f9b71730d79b
Oskar Stark <oskarstark@googlemail.com>
2024-08-29 10:20:53 +0200

    Require PHP 8.2

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 use Ergebnis\PhpCsFixer\Config\Factory;
 use Ergebnis\PhpCsFixer\Config\Rules;
-use Ergebnis\PhpCsFixer\Config\RuleSet\Php74;
+use Ergebnis\PhpCsFixer\Config\RuleSet\Php82;
 
 $header = <<<'HEADER'
 This file is part of the Pushover package.
@@ -24,7 +24,7 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 HEADER;
 
-$ruleSet = Php74::create()->withHeader($header)->withRules(Rules::fromArray([
+$ruleSet = Php82::create()->withHeader($header)->withRules(Rules::fromArray([
     'blank_line_before_statement' => [
         'statements' => [
             'break',
